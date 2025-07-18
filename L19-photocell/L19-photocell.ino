23d963c4349ff767a2108b275fe696d5d2748ac8
t3mina00 <nmine23@student.oulu.fi>
2025-07-18 13:29:19 +0300

    add lesson19

---
+++ L19-photocell/L19-photocell.ino
@@ -0,0 +1,30 @@
+int lightPin = 0;
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+
+int leds = 0;
+
+void setup() {
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+}
+
+void updateShiftRegister() {
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
+
+void loop() {
+  int reading  = analogRead(lightPin); // 0 ~ 1023
+  int numLEDSLit = reading / 57;  
+  if(numLEDSLit > 8) numLEDSLit = 8;
+  leds = 0;
+  for(int i = 0; i < numLEDSLit; i++) {
+    // Sets the i'th bit
+    leds = leds + (1 << i);  
+  }
+  updateShiftRegister();
+}
---
+++ L19-photocell/README.md
@@ -0,0 +1,24 @@
+# Lesson 19 – Photocell
+
+## 📘 Overview
+In this lesson, I learned how to use a photoresistor.
+
+## 🔧 Components Used
+- Arduino Uno R3 Board × 1
+- LED × 8
+- 220 ohm resistors × 8
+- M-M wires × 16
+- 74hc595 IC × 1
+- Photoresistor (Photocell) × 1
+- 1k ohm resistor × 1
+
+## 🎥 Demo Video
+A short demo showing the actual behavior of the project on real hardware.
+
+[![Demo Video](https://img.youtube.com/vi/3R98pKc11V8/0.jpg)](https://youtu.be/3R98pKc11V8)
+
+## 📝 Notes
+### ✅ What I Learned
+- How to use a photoresistor and handle its value.
+
+---
