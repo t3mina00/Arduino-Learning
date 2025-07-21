361656be3ef22390c9e90d7b667c71b97a06a1c8
t3mina00 <nmine23@student.oulu.fi>
2025-07-21 12:41:53 +0300

    add lesson24

---
+++ L24-DC-motors/L24-DC-motors.ino
@@ -0,0 +1,65 @@
+#define ENABLE 5
+#define DIRA 3
+#define DIRB 4
+
+int i;
+ 
+void setup() {
+  pinMode(ENABLE, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Back and forth
+    Serial.println("One way, then reverse");
+    digitalWrite(ENABLE,HIGH); // enable on
+    for (i=0;i<5;i++) {
+        digitalWrite(DIRA,HIGH); //one way
+        digitalWrite(DIRB,LOW);
+        delay(1000);
+        digitalWrite(DIRA,LOW);  //reverse
+        digitalWrite(DIRB,HIGH);
+        delay(1000);
+    }
+    digitalWrite(ENABLE,LOW); // disable
+    delay(2000);
+
+    Serial.println("fast Slow example");
+    // Fast/slow stop 
+    digitalWrite(ENABLE,HIGH); //enable on
+    digitalWrite(DIRA,HIGH); //one way
+    digitalWrite(DIRB,LOW);
+    delay(3000);
+    digitalWrite(ENABLE,LOW); //slow stop
+    delay(1000);
+    digitalWrite(ENABLE,HIGH); //enable on
+    digitalWrite(DIRA,LOW); //one way
+    digitalWrite(DIRB,HIGH);
+    delay(3000);
+    digitalWrite(DIRA,LOW); //fast stop
+    delay(2000);
+
+    Serial.println("PWM full then slow");
+    // PWM example, full speed then slow
+    analogWrite(ENABLE,255); //enable on
+    digitalWrite(DIRA,HIGH); //one way
+    digitalWrite(DIRB,LOW);
+    delay(2000);
+    analogWrite(ENABLE,180); //half speed
+    delay(2000);
+    analogWrite(ENABLE,128); //half speed
+    delay(2000);
+    analogWrite(ENABLE,50); //half speed
+    delay(2000);
+    analogWrite(ENABLE,128); //half speed
+    delay(2000);
+    analogWrite(ENABLE,180); //half speed
+    delay(2000);
+    analogWrite(ENABLE,255); //half speed
+    delay(2000);
+    digitalWrite(ENABLE,LOW); //all done
+    delay(10000);
+}
+   
---
+++ L24-DC-motors/README.md
@@ -0,0 +1,24 @@
+# Lesson 24 – DC Motors
+
+## 📘 Overview
+In this lesson, I learned how to use a DC motor with a power supply module.
+
+## 🔧 Components Used
+- Arduino Uno R3 Board × 1
+- M-M wires × 5
+- L293D IC × 1
+- Fan blade and 3-6v motor × 1
+- Power Supply Module × 1
+- 9V1A adapter × 1
+
+## 🎥 Demo Video
+A short demo showing the actual behavior of the project on real hardware.
+
+[![Demo Video](https://img.youtube.com/vi/6do-IFB-p28/0.jpg)](https://youtube.com/shorts/6do-IFB-p28)
+
+## 📝 Notes
+### ✅ What I Learned
+- How to use a DC motor.
+- How to use a power supply module.
+
+---
